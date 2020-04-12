408b2eaa7bbd757cf8250443ed6332ad7b55e85c
Gabriel Tunaru <gabi.tunaru24@gmail.com>
2020-04-12 18:22:00 +0300

    added comments

--- app.py
+++ app.py
@@ -3,26 +3,26 @@ from flask import Flask, render_template, flash, request, redirect, url_for, Res
 app = Flask(__name__)
 UPLOAD_FOLDER='./static/assets'
 
-class Spot:
+class Spot: #clasa ce detaliaza un loc de parcare, deocamdata are un singur camp care semnifica daca locul este ocupat sau nu
     def __init__(self,taken):
         self.taken=taken
 
-@app.route('/')
+@app.route('/') #ruta folosita pentru interfata web
 def hello():
     spot=Spot(False)
     path=os.path.join(UPLOAD_FOLDER,'image.jpg')
     print(path)
-    return render_template('base.html',spot=spot,image_path=path)
+    return render_template('base.html',spot=spot,image_path=path) #returneaza un template ce se schimba in fuctie de locul de parcare si imaginea primita
 
 
-@app.route('/image',methods=['GET','POST'])
+@app.route('/image',methods=['GET','POST']) #ruta folosita pentru upload de imagine
 def addImage():
     if(request.method=='POST'):
         if 'file' not in request.files:
             return Response("File not present in the request",400)
-    file=request.files['file']
+    file=request.files['file'] 
     path=os.path.join(UPLOAD_FOLDER,'image.jpg')
-    file.save(path)
+    file.save(path) #salvam imaginea pe disk
     print(path)
     return "success"
 
