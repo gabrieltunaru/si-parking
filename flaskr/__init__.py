bb76370048138add0ed6b682c496ea4529255c52
Gabriel Tunaru <gabi.tunaru24@gmail.com>
2020-05-01 15:38:06 +0300

    added docker and request debugging

---
+++ flaskr/__init__.py
@@ -0,0 +1,59 @@
+import os
+from flask import Flask, render_template, flash, request, redirect, url_for, Response
+app = Flask(__name__)
+UPLOAD_FOLDER = './static/assets'
+
+
+class Spot:  # clasa ce detaliaza un loc de parcare, deocamdata are un singur camp care semnifica daca locul este ocupat sau nu
+    def __init__(self, taken):
+        self.taken = taken
+
+    def set_taken(self, taken):
+        self.taken = taken
+
+
+spot = Spot(False)
+
+
+@app.route('/')  # ruta folosita pentru interfata web
+def hello():
+    path = os.path.join(UPLOAD_FOLDER, 'image.jpg')
+    print(path)
+    # returneaza un template ce se schimba in fuctie de locul de parcare si imaginea primita
+    return render_template('base.html', spot=spot, image_path=path)
+
+
+# ruta folosita pentru upload de imagine
+@app.route('/image', methods=['GET', 'POST'])
+def addImage():
+    if(request.method == 'POST'):
+        if 'file' not in request.files:
+            return Response("File not present in the request", 400)
+    file = request.files['file']
+    path = os.path.join(UPLOAD_FOLDER, 'image.jpg')
+    file.save(path)  # salvam imaginea pe disk
+    print(path)
+    return "success"
+
+
+@app.route('/distance', methods=['GET', 'POST'])
+def setDistance():
+    data = request.get_json()
+    print("request data: %s" % data)
+    try:
+        distance1 = data['distance1']
+        print("distance1: %s" % distance1)
+        distance2 = data['distance2']
+        print("distance2: %s" % distance2)
+        if(distance1 < 30 and distance2 < 30):
+            spot.set_taken(True)
+        else:
+            spot.set_taken(False)
+        return "success"
+    except:
+        response="Invalid JSON format: " + str(data)
+        return response,400
+
+
+if __name__ == '__main__':
+    app.run()
--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-FLASK_APP=app.py FLASK_ENV=development flask run
+FLASK_APP=flaskr.py FLASK_ENV=development flask run --host 0.0.0.0
